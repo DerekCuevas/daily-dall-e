8326d51c4d6f9871714c27fd48086af6a65b504c
Derek Cuevas <dccuevas2@gmail.com>
2023-07-16 10:24:18 -1000

    Add trends finder

--- main.ts
+++ main.ts
@@ -1,38 +1,37 @@
 import { OpenAI } from "https://deno.land/x/openai/mod.ts";
 import { config } from "./config.ts";
+import { TrendFinder } from "./trends_finder.ts";
 
-export async function dailyDalle(): Promise<void> {
-  const openai = new OpenAI(config.OPENAI_API_KEY);
+const trendsFinder = new TrendFinder();
+const openai = new OpenAI(config.OPENAI_API_KEY);
 
-  const imagePrompt = "Kelly Slater surfing on the moon, photo realistic.";
-  console.log(`Generating image: ${imagePrompt}`);
+const trends = await trendsFinder.find({ geo: "US" });
+const topThree = trends.slice(0, 3).map((t) => t.query);
 
-  const image = await openai.createImage({
-    prompt: imagePrompt,
-    n: 1,
-    size: "1024x1024",
-  });
-  const imageURL = image.data[0].url;
+const imagePrompt = `An image featuring: ${topThree.join(", ")}`;
+console.log(`Generating image: ${imagePrompt}`);
 
-  console.log(`Generated: ${imageURL}`);
+const image = await openai.createImage({
+  prompt: imagePrompt,
+  n: 1,
+  size: "1024x1024",
+});
+const imageURL = image.data[0].url;
 
-  const archiveFilepath = `./archive/daily-dall-e-${new Date().toISOString()}.png`;
-  const archiveFile = await Deno.open(archiveFilepath, {
-    write: true,
-    create: true,
-  });
+console.log(`Generated: ${imageURL}`);
 
-  const imageResponse = await fetch(imageURL);
-  if (imageResponse.body) {
-    await imageResponse.body.pipeTo(archiveFile.writable);
-  }
+const archiveFilepath = `./archive/daily-dall-e-${new Date().toISOString()}.png`;
+const archiveFile = await Deno.open(archiveFilepath, {
+  write: true,
+  create: true,
+});
 
-  const dailyFilepath = `./daily-dall-e.png`;
-  await Deno.copyFile(archiveFilepath, dailyFilepath);
+const imageResponse = await fetch(imageURL);
+if (imageResponse.body) {
+  await imageResponse.body.pipeTo(archiveFile.writable);
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  await dailyDalle();
-  console.log("Complete.");
-}
+const dailyFilepath = `./daily-dall-e.png`;
+await Deno.copyFile(archiveFilepath, dailyFilepath);
+
+console.log("Complete.");
---
+++ trends_finder.ts
@@ -0,0 +1,40 @@
+import googleTrends from "npm:google-trends-api";
+
+interface TrendingSearch {
+  title: { query: string };
+}
+
+interface TrendingSearchesDay {
+  trendingSearches: TrendingSearch[];
+}
+
+interface GoogleTrendsResponse {
+  trendingSearchesDays: TrendingSearchesDay[];
+}
+
+export interface Filter {
+  geo: string;
+}
+
+export interface Trend {
+  query: string;
+}
+
+export class TrendFinder {
+  async find(filter: Filter): Promise<Trend[]> {
+    const response = await googleTrends.dailyTrends({ geo: filter.geo });
+    const parsed = JSON.parse(response).default as GoogleTrendsResponse;
+
+    console.log(JSON.stringify);
+
+    return parsed.trendingSearchesDays
+      .flatMap((t) => t.trendingSearches)
+      .map((t) => ({ query: t.title.query }));
+  }
+}
+
+// Deno.test(async function testClient() {
+//   const client = new TrendFinder();
+//   const trends = await client.find({ geo: "US" });
+//   console.log(trends);
+// });
